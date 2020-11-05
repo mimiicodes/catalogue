6b9487b220510694cad7d0bd5576d04ffe1cda18
Miracle Ogunleye <60979917+mimiicodes@users.noreply.github.com>
2020-11-05 12:06:26 +0100

    uploaded

---
+++ script.js
@@ -0,0 +1,55 @@
+
+let items = [
+    "transparent handbag", 
+    "crop hoodie", 
+    "crossbag", 
+    "slides", 
+    "dress", 
+    "mini bag", 
+    "jewelry", 
+    "watch", 
+    "waist purse", 
+    "heels"
+];
+
+var available = document.getElementById("text");
+var available2 = document.getElementById("text2");
+var available3 = document.getElementById("text3");
+var available4 = document.getElementById("text4");
+var available5 = document.getElementById("text5");
+var available6 = document.getElementById("text6");
+var available7 = document.getElementById("text7");
+var available8 = document.getElementById("text8");
+var available9 = document.getElementById("text9");
+var available10 = document.getElementById("text10");
+
+function checkingOne () {
+    available.classList.toggle("strike");
+}
+function checkingTwo () {
+    available2.classList.toggle("strike");
+}
+function checkingThree () {
+    available3.classList.toggle("strike");
+}
+function checkingFour () {
+    available4.classList.toggle("strike");
+}
+function checkingFive () {
+    available5.classList.toggle("strike");
+}
+function checkingSix () {
+    available6.classList.toggle("strike");
+}
+function checkingSeven () {
+    available7.classList.toggle("strike");
+}
+function checkingEight () {
+    available8.classList.toggle("strike");
+}
+function checkingNine () {
+    available9.classList.toggle("strike");
+}
+function checkingTen () {
+    available10.classList.toggle("strike");
+}
